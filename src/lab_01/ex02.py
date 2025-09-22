d4c9693abf8357315b7ef6e1ab8a4719e0df11d6
NarushevMikhail <narushevmixail@mail.ru>
2025-09-23 03:55:16 +0400

    1

---
+++ src/lab_01/ex02.py
@@ -0,0 +1,7 @@
+a = float(input('a:'))
+b = float(input('b:'))
+
+sum = a + b
+avg = (a + b)/2
+
+print(f'sum = {sum}; avg = {avg}')
---
+++ src/lab_01/ex05.py
@@ -0,0 +1,7 @@
+fuo = input()
+inizuals = fuo.split()
+a1, a2, a3 = inizuals[0][0], inizuals[1][0], inizuals[2][0]
+dlina = fuo.strip()
+
+print(f'Инициалы: {a1}{a2}{a3}.')
+print(f'Длина: {len(dlina)}')
