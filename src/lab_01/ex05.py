14772af737da2d5be093f69f09d0bd247c23ba5a
NarushevMikhail <narushevmixail@mail.ru>
2025-09-23 14:48:41 +0400

    100

--- src/lab_01/ex05.py
+++ src/lab_01/ex05.py
@@ -1,7 +1,10 @@
 fuo = input('ФИО:')
 inizuals = fuo.split()
+print(inizuals)
 a1, a2, a3 = inizuals[0][0], inizuals[1][0], inizuals[2][0]
-dlina = fuo.strip()
+inizuals1 = ''.join(inizuals)
 
 print(f'Инициалы: {a1}{a2}{a3}.')
-print(f'Длина: {len(dlina)}')
+print(f'Длина: {len(inizuals1)+2}')
+
+
